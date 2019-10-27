4bff68183d39536ff44c5fbb1cae09e21017fac9
unknown <nikos.a.constantinou@gmail.com>
2019-10-27 21:14:03 +0000

    Updates

---
+++ precache-manifest.13254ffba2e21a398f8a309efc3dd807.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34a45c2ed61204af135bc40ce019adfc",
+    "url": "/farmdrop-code-test-js/index.html"
+  },
+  {
+    "revision": "1150ddc1fb014ddb28db",
+    "url": "/farmdrop-code-test-js/static/css/main.7ef4fe92.chunk.css"
+  },
+  {
+    "revision": "43889c6be7370c22ccee",
+    "url": "/farmdrop-code-test-js/static/js/2.51bfd4cb.chunk.js"
+  },
+  {
+    "revision": "1150ddc1fb014ddb28db",
+    "url": "/farmdrop-code-test-js/static/js/main.6d7506fa.chunk.js"
+  },
+  {
+    "revision": "2b2441759466213d0c42",
+    "url": "/farmdrop-code-test-js/static/js/runtime-main.5ad9e0dc.js"
+  },
+  {
+    "revision": "eb86888a40fc42b3c03ba9657f15fa96",
+    "url": "/farmdrop-code-test-js/static/media/basket.eb86888a.svg"
+  },
+  {
+    "revision": "81f2db26cfe0901aa027243671a7ecbf",
+    "url": "/farmdrop-code-test-js/static/media/chevron.81f2db26.svg"
+  },
+  {
+    "revision": "41c7f6189c9aa6dc51a491582c6716a4",
+    "url": "/farmdrop-code-test-js/static/media/minus.41c7f618.svg"
+  },
+  {
+    "revision": "399c28176e61ad4de46529d1e76aa7a0",
+    "url": "/farmdrop-code-test-js/static/media/plus.399c2817.svg"
+  }
+]);
