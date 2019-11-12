1b48c41f46508e4ad68dee9aa442ee542dbc2306
unknown <nikos.a.constantinou@gmail.com>
2019-11-12 13:58:02 +0000

    sa

--- farmdrom-product/src/store/store.js
+++ farmdrom-product/src/store/store.js
@@ -5,39 +5,8 @@ import { postReducer } from './reducer/postReducer'
 
 const middlweare = [thunk]
 
-const initialState = { products: [], basket: [] }
-
-// const postReducer = (state = initialState, actions) => {
-//   if (actions.type === 'FETCH_POST') {
-//     return { ...state, products: actions.products }
-//   }
-//   if (actions.type === 'ADD_ITEM_IN_BASKET') {
-//     console.log(actions.basket)
-//     return { ...state, basket: state.basket.concat(actions.basket) }
-//   }
-//   if (actions.type === 'PLUS_QUANTITY') {
-//     const newStatePlus = [...state.basket]
-//     newStatePlus[actions.indexBasket].quantity ++
-//     return { ...state, basket: newStatePlus }
-//   }
-//   if (actions.type === 'MINUS_QUANTITY') {
-//     let newStateMinus = [...state.basket]
-//     if (newStateMinus[actions.indexBasket].quantity === 1) {
-//       newStateMinus.splice(actions.indexBasket)
-//       return  {...state, basket: newStateMinus }
-//     }
-//     if (newStateMinus[actions.indexBasket].quantity > 0) {
-//       newStateMinus[actions.indexBasket].quantity --
-//       return { ...state, basket: newStateMinus }
-//     }
-//   }
-//   return state
-// }
-
-
 const store = createStore(
   postReducer,
-  // applyMiddleware(...middlweare)
   compose(
     applyMiddleware(...middlweare),
     window.navigator.userAgent.includes('Chrome') ?
