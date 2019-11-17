4c6125f8dc006e33320a2cc2a5f799a56d99307c
unknown <nikos.a.constantinou@gmail.com>
2019-11-17 12:36:22 +0000

    remove junk comments

--- app/src/store/actions/types.js
+++ app/src/store/actions/types.js
@@ -2,6 +2,3 @@ export const ADD_ITEM_IN_BASKET ='ADD_ITEM_IN_BASKET';
 export const FETCH_POST ='FETCH_POST';
 export const PLUS_QUANTITY='PLUS_QUANTITY';
 export const MINUS_QUANTITY='MINUS_QUANTITY';
-
-// export const ADD_ITEM_IN_ORDER ='ADD_ITEM_IN_ORDER'
-// export const PLUS_OR_MAINS_Q = 'PLUS_OR_MAINS_Q'
--- app/src/store/store.js
+++ app/src/store/store.js
@@ -1,8 +1,6 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import { postReducer } from './reducer/postReducer'
-// import { ADD_ITEM_IN_BASKET, FETCH_POST, PLUS_QUANTITY, MINUS_QUANTITY  } from './actions/types'
-
 const middlweare = [thunk]
 
 const store = createStore(
