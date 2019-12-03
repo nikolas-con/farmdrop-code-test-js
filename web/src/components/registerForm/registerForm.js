82b8e6e837d0c676e19533fd55eace523dd6fa12
unknown <nikos.a.constantinou@gmail.com>
2019-12-03 20:35:20 +0000

    setup axios and cread register form

--- web/src/components/registerForm/registerForm.js
+++ web/src/components/registerForm/registerForm.js
@@ -1,12 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './registerFrom.scss'
+import axios from 'axios'
 
 const RegisterForm = () => {
+  const userInfo = {
+    first_name: null,
+    last_name: null,
+    email: null,
+    password: null,
+    image_urls:[]
+  }
+  const [ user, setuser ] = useState({...userInfo})
+  const onChangeInput = e => {
+    setuser({ ...user, [e.target.name]: e.target.value })
+  }
+
+  const onClick = async (e) => {
+    e.preventDefault();
+    let post = {
+      method: 'put',
+      url: '/users/create',
+      data: user
+    }
+    let result = await axios(post)
+    localStorage.setItem("token", result.data.jwt)
+  }
   return (
-    <div>
-      <input type="text" name="fname" placeholder="First name"/>
-      <input type="text" name="lname" placeholder="Last name"/>
-      <input type="email" name="email" placeholder="Email"/>
-      <input type="password" name="password" placeholder="Password"/>
+    <div className="register-form">
+      <form onSubmit={onClick}>
+        <p><input type="text" onChange={onChangeInput} name="first_name" placeholder="First name"/></p>
+        <p><input type="text" onChange={onChangeInput} name="last_name" placeholder="Last name"/></p>
+        <p><input type="email" onChange={onChangeInput} name="email" placeholder="Email"/></p>
+        <p><input type="password" onChange={onChangeInput} name="password" placeholder="Password"/></p>
+      </form>
     </div>
    );
 }
